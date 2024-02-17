fc54742b9aef3119a22894d3421efa4cbb85b817
Inam Ul Haq <111003676+inamtyulhaq@users.noreply.github.com>
2024-02-17 22:47:33 +0500

    Create 1000. A+B Problem.c

---
+++ Timus-Online-Judge /Volume 1 (1000–1099)/1000. A+B Problem.c
@@ -0,0 +1,18 @@
+// Question:
+/*Calculate a + b
+Input a and b
+Outputa+b
+Use + operator.*/
+
+// Explanation:
+/*Simply took a number from the user/system and simply printed their sum.*/
+
+// Solution:
+#include <stdio.h>
+
+int main() {
+    int a, b;
+    scanf("%d %d", &a, &b);
+    printf("%d", a+b);
+    return 0;
+}
