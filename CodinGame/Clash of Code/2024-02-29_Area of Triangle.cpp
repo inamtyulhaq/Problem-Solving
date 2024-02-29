0c0fe0f6013bcbb2c1e65257549bfb5ca97e22e0
Inam Ul Haq <111003676+inamtyulhaq@users.noreply.github.com>
2024-02-29 10:30:05 +0500

    Create 2024-02-29_Area of Triangle.cpp

---
+++ CodinGame/Clash of Code/2024-02-29_Area of Triangle.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+
+int main()
+{
+    int a;
+    int b;
+    cin >> a >> b; cin.ignore();
+
+    int c = 180 -a - b;
+    cout << c << endl;
+}
