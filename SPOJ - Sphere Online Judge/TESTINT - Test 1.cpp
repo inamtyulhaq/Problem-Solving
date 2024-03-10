a10748cd995803faeb1fd00398dece1984e768fa
Inam Ul Haq <111003676+inamtyulhaq@users.noreply.github.com>
2024-03-10 14:29:46 +0500

    Update and rename file to TESTINT - Test 1.cpp

---
+++ SPOJ - Sphere Online Judge/TESTINT - Test 1.cpp
@@ -0,0 +1,18 @@
+// Question:
+// https://www.spoj.com/problems/TESTINT/
+
+// Explanation:
+// simply too two numbers and added them.
+
+// Solution:
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+	int a, b;
+    cin >> a >> b;
+    cout << a+b << endl;
+
+	return 0;
+}
