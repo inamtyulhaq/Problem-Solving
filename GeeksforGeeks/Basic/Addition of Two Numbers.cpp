70da982b06d076b705863634bc59bb2bf709e784
Inam Ul Haq <111003676+inamtyulhaq@users.noreply.github.com>
2024-08-01 11:15:15 +0500

    Create Addition of Two Numbers.cpp

---
+++ GeeksforGeeks/Basic/Addition of Two Numbers.cpp
@@ -0,0 +1,33 @@
+// https://www.geeksforgeeks.org/problems/addition-of-two-numbers0812/1
+
+// return the sum
+
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+class Solution{   
+public:
+    int addition(int A, int B){
+        // code here 
+        return A+B;
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int A, B;
+        cin >> A >> B;
+        Solution ob;
+        cout << ob.addition(A,B) << endl;
+    }
+    return 0; 
+} 
+
+// } Driver Code Ends
